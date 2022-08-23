05b525dbddf8a98f4835955cecb79b1053ff88ae
Igor Faria <itfaria@gmail.com>
2022-08-23 08:40:25 +0100

    Some methods were refactored  in the Product model

--- app/model/resource/Product.js
+++ app/model/resource/Product.js
@@ -15,78 +15,45 @@ export class Product extends Model {
     
     }
 
-    async getProduct(id){
-        const hasProducts = ('data' in this.state && 'products' in this.state.data && this.state.data.products.length)
-
-        if(!hasProducts) {
-            await this.get()
-            .then(t => {
-                this.state.data = t
-                t.data.products.forEach(product => {
-                    if('id' in product && product.id == id){
-                        this.setState({data: product})
-                    }
-                })
-                return this
-            })
-            .catch(c => {
-                return c
-            }) 
-            .finally(f => {
-                return f
-            })
-        } else {
-            this.state.data.products.forEach(product => {
-                if('id' in product && product.id == id){
-                    this.state.data = product
-                }
-            })
-        }
-       
-        return this.getData();
-    }
-
     getId(){
-        const data = this.getData()
-        if('id' in data) return data.id;
-        return 0;
+        return this.getData()?.id ?? 0
     }
 
     getData(){
-        const params = this.props.route ? this.props.route.params : {}
-        const state = this.state.data && this.state.data.length ? this.state.data : {}
-        const props = this.props && this.props.data ? this.props.data : {}
-        const data = {...props, ...params, ...state} 
+        const params = this.props?.route ?? {}
+        const state = this.state?.data ?? {}
+        const props = this.props?.data ?? {}
+        const data = {...params, ...props, ...state} 
         return data;
     }
 
     getName(){
-        const data = this.getData();
-        if('name' in data && Array.isArray(data.name) && 'value' in data.name[AppConfig.lang]) return data.name[AppConfig.lang].value; 
-        return 'No name';
+        return this.getData()?.name[AppConfig.lang]?.value ?? 'No name'
     }
 
     getPrice(){
         const data = this.getData();
-        if('price' in data) return this.formatPrice(data.price);
-        return '';
+        return ('price' in data) ? this.formatPrice(data.price) : ''
     }
 
     getCover(){
         const data = this.getData()
-        if('id_default_image' in data){
-            return this.getImageUri(data.id, data.id_default_image)
-        }
+        return ('id_default_image' in data) ? this.getImageUri(data.id, data.id_default_image) : false
     }
 
     getImageUri(productId, imageId){
         return this.createRequestLink(false, `images/products/${productId}/${imageId}`)
     }
 
+    getDescription(){
+        return this.getData()?.description[AppConfig.lang]?.value ?? '' 
+    }
+
+
     getImages(){
         const data = this.getData()
         const images = []
-        if('associations' in data && 'images' in data.associations){
+        if(data?.associations){
             data.associations.images.map(img => {
                 images.push({
                     id: img.id,
@@ -98,14 +65,11 @@ export class Product extends Model {
     }
 
     goToProductPage = () => {
-        const id = ('id' in this.getData()) ? this.getData().id : 0;
-        console.log(this.getData());
-        return id;
+        const id = this.getId();
      }
 
     addProductToCart = () => {
-        const id = ('id' in this.getData()) ? this.getData().id : 0;
-        alert('Add product to cart: ' + id);
+        alert('Add product to cart: ' + this.getId());
     }
 
     render(){
