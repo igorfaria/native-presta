6e6226c6ac21e3e98c57940a2b9da6c20eb78b29
Igor Faria <itfaria@gmail.com>
2022-08-27 18:52:50 +0100

    Changed values from props to state values

--- app/view/cart/CartPage.js
+++ app/view/cart/CartPage.js
@@ -8,14 +8,25 @@ export class CartPage extends Component {
 
     constructor(props){
         super(props)
+        this.state = {
+            items: []
+        }
+    }
+
+    componentDidUpdate = () => this.setItemsState()
+
+    setItemsState(){
+        if(this.state.items !== this.props.items){
+            this.setState({items: this.props.items})
+        }
     }
 
     render() {
-        if(this.props.items.length == 0) return <></>
+        if(this.state.items.length == 0) return <></>
         return (
             <ScrollView>
                 { 
-                    this.props.items.map( (value ,index) => <CartRow 
+                    this.state.items.map( (value ,index) => <CartRow 
                         key={index} 
                         quantity={value.quantity} 
                         product={new Product(value.item)} />
